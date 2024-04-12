a42e7b822e2d1f25fb86ac7a68f58007c1dc24e7
eundeang <111877048+eundeang@users.noreply.github.com>
2024-04-12 17:02:44 +0900

    [INIT] 백엔드 프로젝트 생성

---
+++ codekids_BE/src/main/java/oop/codekids/CodekidsApplication.java
@@ -0,0 +1,13 @@
+package oop.codekids;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodekidsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodekidsApplication.class, args);
+    }
+
+}
---
+++ codekids_BE/src/test/java/oop/codekids/CodekidsApplicationTests.java
@@ -0,0 +1,13 @@
+package oop.codekids;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodekidsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
