fe031eda75dafa742200c3bbeb67cb53d3d480b1
eundeang <111877048+eundeang@users.noreply.github.com>
2024-05-15 16:24:41 +0900

    [FEAT] 이미지를 보내주기 위한 Image Entity 생성

---
+++ codekids_BE/src/main/java/oop/codekids/entity/Image.java
@@ -0,0 +1,22 @@
+package oop.codekids.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Image {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String originalFileName;
+
+    @Column(nullable = false)
+    private String storedFilePath;
+
+}
