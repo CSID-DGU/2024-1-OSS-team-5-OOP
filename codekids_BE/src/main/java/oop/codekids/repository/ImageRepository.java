8e97d01ca14971c530d9bf8c8ce83f906d458a4f
eundeang <111877048+eundeang@users.noreply.github.com>
2024-05-16 21:13:08 +0900

    [CHORE] validation 추가

--- codekids_BE/src/main/java/oop/codekids/repository/ImageRepository.java
+++ codekids_BE/src/main/java/oop/codekids/repository/ImageRepository.java
@@ -1,7 +1,7 @@
 package oop.codekids.repository;
 
-import oop.codekids.entity.Image;
+import oop.codekids.entity.ProblemImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ImageRepository extends JpaRepository<Image, Long> {
+public interface ImageRepository extends JpaRepository<ProblemImage, Long> {
 }
