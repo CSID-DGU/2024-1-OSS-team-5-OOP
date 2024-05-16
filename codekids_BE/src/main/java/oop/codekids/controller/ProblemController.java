8236bd64e212500b325efe376525c4fa24557ede
eundeang <111877048+eundeang@users.noreply.github.com>
2024-05-16 20:51:02 +0900

    [REFACT] 사용하지 않는 import 제거

--- codekids_BE/src/main/java/oop/codekids/controller/ProblemController.java
+++ codekids_BE/src/main/java/oop/codekids/controller/ProblemController.java
@@ -1,14 +1,11 @@
 package oop.codekids.controller;
 
 import lombok.RequiredArgsConstructor;
-import oop.codekids.dto.ProblemDto;
 import oop.codekids.dto.ProblemsDto;
 import oop.codekids.service.ProblemService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
