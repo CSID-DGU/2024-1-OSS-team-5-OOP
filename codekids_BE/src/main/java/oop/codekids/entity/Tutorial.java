c25d3257ed9ae2ba96ec7557a83237277993367e
eundeang <111877048+eundeang@users.noreply.github.com>
2024-05-16 22:23:03 +0900

    [FEAT] Tutorial 앤티티 생성

---
+++ codekids_BE/src/main/java/oop/codekids/entity/Tutorial.java
@@ -0,0 +1,23 @@
+package oop.codekids.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import oop.codekids.Concept;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "tutorial")
+public class Tutorial {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long Id;
+    private Concept concept;
+
+    @Builder
+    Tutorial(Concept concept) {
+        this.concept = concept;
+    }
+}
