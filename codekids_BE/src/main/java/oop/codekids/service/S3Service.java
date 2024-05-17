a64ddb835d48ecd6d6ee598347d96832a9fb1171
eundeang <111877048+eundeang@users.noreply.github.com>
2024-05-17 15:59:33 +0900

    [FEAT] S3에거 URL 받아오는 getImageUrl 구현

--- codekids_BE/src/main/java/oop/codekids/service/S3Service.java
+++ codekids_BE/src/main/java/oop/codekids/service/S3Service.java
@@ -3,35 +3,57 @@ package oop.codekids.service;
 import oop.codekids.config.AWSConfig;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 import software.amazon.awssdk.core.sync.RequestBody;
 import software.amazon.awssdk.services.s3.S3Client;
 import software.amazon.awssdk.services.s3.model.DeleteObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetUrlRequest;
 import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.model.S3Exception;
+
 
 import java.io.IOException;
+import java.net.URL;
 import java.util.UUID;
 
 @Component
 public class S3Service {
     private final String bucketName;
     private final AWSConfig awsConfig;
+    private final S3Client s3Client;
+
 
     public S3Service(@Value("${aws-property.s3-bucket-name}") final String bucketName, AWSConfig awsConfig) {
         this.bucketName = bucketName;
         this.awsConfig = awsConfig;
+        this.s3Client = awsConfig.getS3Client();
+    }
+
+
+    public void getImageUrl(String key) {
+        try {
+            GetUrlRequest request = GetUrlRequest.builder()
+                    .bucket(bucketName)
+                    .key(key)
+                    .build();
+
+            URL url = s3Client.utilities().getUrl(request);
+            System.out.println("The URL for  " + key + " is " + url);
+        } catch (S3Exception e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+            System.exit(1);
+        }
+
     }
 
 
     public String uploadImage(String directoryPath, MultipartFile image) throws IOException {
-        final String key = directoryPath + generateImageFileName();
-        final S3Client s3Client = awsConfig.getS3Client();
+        final String key = directoryPath + image.getOriginalFilename();
 
         PutObjectRequest request = PutObjectRequest.builder()
                 .bucket(bucketName)
                 .key(key)
-                .contentType(image.getContentType())
+                .contentType("image/png")
                 .contentDisposition("inline")
                 .build();
 
@@ -39,6 +61,8 @@ public class S3Service {
         s3Client.putObject(request, requestBody);
         return key;
     }
+
+
 
     public void deleteImage(String key) throws IOException {
         final S3Client s3Client = awsConfig.getS3Client();
@@ -56,3 +80,4 @@ public class S3Service {
     }
 
 }
+
