37f30b667b39742823f2941287d8858cf8d155a4
eundeang <111877048+eundeang@users.noreply.github.com>
2024-05-17 17:01:58 +0900

    [TEST] Problem문제 전부 추가 테스트 코드 작성

--- codekids_BE/src/test/java/oop/codekids/CodekidsApplicationTests.java
+++ codekids_BE/src/test/java/oop/codekids/CodekidsApplicationTests.java
@@ -32,34 +32,54 @@ class CodekidsApplicationTests {
     }
 
     @Test
-    @DisplayName("데이터 저장하기")
-    void createData() {
+    @DisplayName("문제 데이터 전부 저장하기")
+    void saveAllProblem() {
         Problem workBook1 = Problem.builder()
-                .concept(oop.codekids.Concept.ABSTRACT)
-                .problemTitle("나는 낭만 고양이!")
+                .title("업앤다운 게임 만들기")
+                .concept(Concept.ENCAPSULATION)
+                .imageUrl("https://codekids-bucket.s3.ap-northeast-2.amazonaws.com/problem/upAndDown.png")
                 .build();
 
         Problem workBook2 = Problem.builder()
-                .concept(oop.codekids.Concept.ABSTRACT)
-                .problemTitle("불이 났어요!")
+                .title("자판기 유지보수하기")
+                .concept(Concept.ENCAPSULATION)
+                .imageUrl("https://codekids-bucket.s3.ap-northeast-2.amazonaws.com/problem/vendingMachine.png")
                 .build();
 
         Problem workBook3 = Problem.builder()
-                .concept(oop.codekids.Concept.ABSTRACT)
-                .problemTitle("빠빵 차가 지나갑니당!")
+                .title("도형 넓이 계산하기")
+                .concept(Concept.ABSTRACT)
+                .imageUrl("https://codekids-bucket.s3.ap-northeast-2.amazonaws.com/problem/car.png")
                 .build();
+
         Problem workBook4 = Problem.builder()
-                .concept(oop.codekids.Concept.POLYMORPHISM)
-                .problemTitle("도형이 뭘까요?")
+                .title("자동차 움직이기")
+                .concept(Concept.ABSTRACT)
+                .imageUrl("https://codekids-bucket.s3.ap-northeast-2.amazonaws.com/problem/car.png")
+                .build();
+
+        Problem workBook5 = Problem.builder()
+                .title("동물친구들과 놀기")
+                .concept(Concept.POLYMORPHISM)
+                .imageUrl("https://codekids-bucket.s3.ap-northeast-2.amazonaws.com/problem/animalFriends.png")
+                .build();
+
+        Problem workBook6 = Problem.builder()
+                .title("연극 배우 공통점 찾기")
+                .concept(Concept.POLYMORPHISM)
+                .imageUrl("https://codekids-bucket.s3.ap-northeast-2.amazonaws.com/problem/show.png")
                 .build();
 
         problemRepository.save(workBook1);
         problemRepository.save(workBook2);
         problemRepository.save(workBook3);
         problemRepository.save(workBook4);
+        problemRepository.save(workBook5);
+        problemRepository.save(workBook6);
 
-        Assertions.assertThat(problemRepository.findAll()).hasSize(4);
+        Assertions.assertThat(problemRepository.findAll()).hasSize(6);
     }
+
     @Test
     void deleteData() {
         problemRepository.deleteAll();
@@ -72,13 +92,13 @@ class CodekidsApplicationTests {
     @DisplayName("튜토리얼 데이터 넣기")
     void addTutorial() {
         Tutorial tutorial1 = Tutorial.builder()
-                .concept(oop.codekids.Concept.ABSTRACT)
+                .concept(Concept.POLYMORPHISM)
                 .build();
         Tutorial tutorial2 = Tutorial.builder()
-                .concept(oop.codekids.Concept.ENCAPSULATION)
+                .concept(Concept.ENCAPSULATION)
                 .build();
         Tutorial tutorial3 = Tutorial.builder()
-                .concept(oop.codekids.Concept.POLYMORPHISM)
+                .concept(Concept.POLYMORPHISM)
                 .build();
 
         tutorialRepository.save(tutorial1);
@@ -87,9 +107,10 @@ class CodekidsApplicationTests {
 
         Assertions.assertThat(tutorialRepository.findAll()).hasSize(3);
     }
+
     @Test
     @DisplayName("튜토리얼 전체 조회")
-    void getAllTutorial(){
+    void getAllTutorial() {
         List<Tutorial> tutorials = tutorialRepository.findAll();
         Assertions.assertThat(tutorials).isNotNull();
     }
@@ -107,8 +128,7 @@ class CodekidsApplicationTests {
         // 파일이 존재하는지 확인
         if (!file.exists()) {
             throw new IOException("File not found at " + filePath);
-        }
-        else {
+        } else {
             // 파일 내용을 바이트 배열로 읽기
             FileInputStream input = new FileInputStream(file);
             byte[] fileContent = input.readAllBytes();
@@ -124,22 +144,21 @@ class CodekidsApplicationTests {
 
     }
 
-    Problem postImageWithData(MultipartFile image)  {
-        try{
+    Problem postImageWithData(MultipartFile image) {
+        try {
 
-            s3Service.uploadImage("problem/",image);
+            s3Service.uploadImage("problem/", image);
             Problem problem5 = Problem.builder()
-                    .problemTitle("강아지는 냐옹, 고양이는 멍멍?")
+                    .title("강아지는 냐옹, 고양이는 멍멍?")
                     .concept(oop.codekids.Concept.ABSTRACT)
                     .imageUrl(image.getOriginalFilename())
                     .build();
             problemRepository.save(problem5);
             return problem5;
-        }catch (RuntimeException | IOException e){
+        } catch (RuntimeException | IOException e) {
             throw new RuntimeException(e.getMessage());
         }
     }
-
 
 
 }
