ca9519f4b34e01014fcd483b505d71e3b626c88f
eundeang <111877048+eundeang@users.noreply.github.com>
2024-05-17 14:22:24 +0900

    [TEST] Image 업로드 기능 Test

--- codekids_BE/src/test/java/oop/codekids/CodekidsApplicationTests.java
+++ codekids_BE/src/test/java/oop/codekids/CodekidsApplicationTests.java
@@ -1,19 +1,18 @@
 package oop.codekids;
 
+
 import oop.codekids.entity.Problem;
 import oop.codekids.entity.Tutorial;
 import oop.codekids.repository.ProblemRepository;
 import oop.codekids.repository.TutorialRepository;
 import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Profile;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
-
+import org.springframework.web.multipart.MultipartFile;
 import java.util.List;
 
 @SpringBootTest
@@ -21,7 +20,7 @@ import java.util.List;
 class CodekidsApplicationTests {
 
     @Autowired
-    private ProblemRepository workBookRepository;
+    private ProblemRepository problemRepository;
 
     @Test
     void contextLoads() {
@@ -31,34 +30,34 @@ class CodekidsApplicationTests {
     @DisplayName("데이터 저장하기")
     void createData() {
         Problem workBook1 = Problem.builder()
-                .concept(Concept.ABSTRACT)
+                .concept(oop.codekids.Concept.ABSTRACT)
                 .problemTitle("나는 낭만 고양이!")
                 .build();
 
         Problem workBook2 = Problem.builder()
-                .concept(Concept.ABSTRACT)
+                .concept(oop.codekids.Concept.ABSTRACT)
                 .problemTitle("불이 났어요!")
                 .build();
 
         Problem workBook3 = Problem.builder()
-                .concept(Concept.ABSTRACT)
+                .concept(oop.codekids.Concept.ABSTRACT)
                 .problemTitle("빠빵 차가 지나갑니당!")
                 .build();
         Problem workBook4 = Problem.builder()
-                .concept(Concept.POLYMORPHISM)
+                .concept(oop.codekids.Concept.POLYMORPHISM)
                 .problemTitle("도형이 뭘까요?")
                 .build();
 
-        workBookRepository.save(workBook1);
-        workBookRepository.save(workBook2);
-        workBookRepository.save(workBook3);
-        workBookRepository.save(workBook4);
+        problemRepository.save(workBook1);
+        problemRepository.save(workBook2);
+        problemRepository.save(workBook3);
+        problemRepository.save(workBook4);
 
-        Assertions.assertThat(workBookRepository.findAll()).hasSize(4);
+        Assertions.assertThat(problemRepository.findAll()).hasSize(4);
     }
     @Test
     void deleteData() {
-        workBookRepository.deleteAll();
+        problemRepository.deleteAll();
     }
 
     @Autowired
@@ -68,13 +67,13 @@ class CodekidsApplicationTests {
     @DisplayName("튜토리얼 데이터 넣기")
     void addTutorial() {
         Tutorial tutorial1 = Tutorial.builder()
-                .concept(Concept.ABSTRACT)
+                .concept(oop.codekids.Concept.ABSTRACT)
                 .build();
         Tutorial tutorial2 = Tutorial.builder()
-                .concept(Concept.ENCAPSULATION)
+                .concept(oop.codekids.Concept.ENCAPSULATION)
                 .build();
         Tutorial tutorial3 = Tutorial.builder()
-                .concept(Concept.POLYMORPHISM)
+                .concept(oop.codekids.Concept.POLYMORPHISM)
                 .build();
 
         tutorialRepository.save(tutorial1);
@@ -90,6 +89,32 @@ class CodekidsApplicationTests {
         Assertions.assertThat(tutorials).isNotNull();
     }
 
+
+
+
+    @Test
+    void testPostImageWithData()  {
+
+        MockMultipartFile image = new MockMultipartFile("image", "animal.png", "image/png", "anima.png".getBytes());
+
+        Problem target = postImageWithData(image);
+
+        Assertions.assertThat(target.getImageUrl()).isEqualTo("animal.png");
+    }
+
+    Problem postImageWithData(MultipartFile image)  {
+        try{
+            Problem problem5 = Problem.builder()
+                    .problemTitle("강아지는 냐옹, 고양이는 멍멍?")
+                    .concept(oop.codekids.Concept.ABSTRACT)
+                    .imageUrl(image.toString())
+                    .build();
+            problemRepository.save(problem5);
+            return problem5;
+        }catch (RuntimeException e){
+            throw new RuntimeException(e.getMessage());
+        }
+    }
 
 
 }
