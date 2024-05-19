fbfdb41146d70ad725cdbbef2cc75448a58725ab
eundeang <111877048+eundeang@users.noreply.github.com>
2024-05-20 03:54:26 +0900

    [FEAT] TutorialRepo 개념을 가지고 데이터 찾기 설계

--- codekids_BE/src/main/java/oop/codekids/repository/TutorialRepository.java
+++ codekids_BE/src/main/java/oop/codekids/repository/TutorialRepository.java
@@ -1,7 +1,9 @@
 package oop.codekids.repository;
 
+import oop.codekids.Concept;
 import oop.codekids.entity.Tutorial;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    Tutorial findByConcept(Concept concept);
 }
