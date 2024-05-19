ab99b5e41f59e3b2294e9a1a869a4652b6eb2e5d
eundeang <111877048+eundeang@users.noreply.github.com>
2024-05-20 03:52:54 +0900

    [FEAT] getTutorialApi 컨트롤러 추가

--- codekids_BE/src/main/java/oop/codekids/controller/TutorialController.java
+++ codekids_BE/src/main/java/oop/codekids/controller/TutorialController.java
@@ -2,11 +2,14 @@ package oop.codekids.controller;
 
 import jakarta.persistence.Id;
 import lombok.RequiredArgsConstructor;
+import oop.codekids.dto.TutorialDetailDto;
 import oop.codekids.dto.TutorialsDto;
+import oop.codekids.entity.TutorialDetail;
 import oop.codekids.service.TutorialService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -17,5 +20,9 @@ public class TutorialController {
     @GetMapping("/getAllTutorial")
     public ResponseEntity<TutorialsDto> getAllTutorial() {
         return ResponseEntity.ok(tutorialService.getAllTutorial());
+    }
+
+    @GetMapping("getTutorial/{concept_s}")
+    public ResponseEntity<TutorialDetailDto> getTutorial(@PathVariable String concept_s) {return ResponseEntity.ok(tutorialService.getTutorialDetail(concept_s));
     }
 }
