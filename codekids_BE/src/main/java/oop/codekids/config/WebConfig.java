4e01dbfe11349f0e93bfeef1fbfe9ce8a1d6a0e8
eundeang <111877048+eundeang@users.noreply.github.com>
2024-05-20 01:57:43 +0900

    [CHORE] @EnableWebMvc 추가

--- codekids_BE/src/main/java/oop/codekids/config/WebConfig.java
+++ codekids_BE/src/main/java/oop/codekids/config/WebConfig.java
@@ -2,18 +2,19 @@ package oop.codekids.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://localhost:3000")
                 .allowCredentials(true)
                 .maxAge(3600)
                 .allowedHeaders("Authorization", "Content-Type")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .exposedHeaders("Custom-Header", "Custom-Value");
 
     }
 
