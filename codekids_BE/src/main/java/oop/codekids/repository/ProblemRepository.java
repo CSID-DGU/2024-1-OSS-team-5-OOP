0cd55bc573deef59bf3f2d52da53cd6aa9f2ebef
eundeang <111877048+eundeang@users.noreply.github.com>
2024-05-27 23:37:23 +0900

    [FEAT] 컨셉별 문제 조회 api Repository 구현

--- codekids_BE/src/main/java/oop/codekids/repository/ProblemRepository.java
+++ codekids_BE/src/main/java/oop/codekids/repository/ProblemRepository.java
@@ -1,11 +1,14 @@
 package oop.codekids.repository;
 
+import oop.codekids.Concept;
 import oop.codekids.entity.Problem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ProblemRepository extends JpaRepository<Problem, Long> {
+    List<Problem> findAllByConcept(Concept concept);
 }
