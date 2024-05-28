45bc19147dbb6eca5c842c026074adc7454630d8
eundeang <111877048+eundeang@users.noreply.github.com>
2024-05-28 11:43:27 +0900

    [FEAT] QuizType, ox, multi 구현

---
+++ codekids_BE/src/main/java/oop/codekids/entity/Quiz.java
@@ -0,0 +1,33 @@
+package oop.codekids.entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import oop.codekids.QuizType;
+
+import java.util.List;
+import java.util.Optional;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "quiz")
+public class Quiz {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @Column(name = "quiz_type")
+    private QuizType quizType;
+    private String title;
+    private String description;
+    private int answer;
+    @Builder
+    public Quiz(QuizType quizType, String title, Optional<List<String>> multiChoose, String description, int answer){
+        this.quizType = quizType;
+        this.title = title;
+        this.description = description;
+        this.answer = answer;
+    }
+
+}
