e67c795d5a018e3f349e4cf4e17d0eb651ca4b92
eundeang <111877048+eundeang@users.noreply.github.com>
2024-05-28 13:08:43 +0900

    [FEAT] Quiz Controller 생성

---
+++ codekids_BE/src/main/java/oop/codekids/controller/QuizController.java
@@ -0,0 +1,12 @@
+package oop.codekids.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/quiz")
+@RequiredArgsConstructor
+public class QuizController {
+
+}
