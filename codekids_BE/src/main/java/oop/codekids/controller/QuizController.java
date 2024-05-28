620d2b9d98340cfd42a17cf638c8a8969d7c7585
eundeang <111877048+eundeang@users.noreply.github.com>
2024-05-28 20:21:36 +0900

    [FEAT] QuizController 구현

--- codekids_BE/src/main/java/oop/codekids/controller/QuizController.java
+++ codekids_BE/src/main/java/oop/codekids/controller/QuizController.java
@@ -1,12 +1,23 @@
 package oop.codekids.controller;
 
 import lombok.RequiredArgsConstructor;
+import oop.codekids.dto.QuizListDto;
+import oop.codekids.service.QuizService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/quiz")
 @RequiredArgsConstructor
 public class QuizController {
 
+    private final QuizService quizService;
+
+    @GetMapping("/getQuizList")
+    public ResponseEntity<QuizListDto> getQuizList(@RequestParam("concept")String concept_s){
+        return ResponseEntity.ok(quizService.getQuizList(concept_s));
+    }
 }
