3c68f5a06d2d81790d71ee5a29efbab4ca96bd6d
eundeang <111877048+eundeang@users.noreply.github.com>
2024-05-28 20:23:13 +0900

    [FEAT] TutorialDetailRepository 구현

--- codekids_BE/src/main/java/oop/codekids/repository/TutorialDetailRepository.java
+++ codekids_BE/src/main/java/oop/codekids/repository/TutorialDetailRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+
 public interface TutorialDetailRepository extends JpaRepository<TutorialDetail, Long> {
     List<TutorialDetail> findAllByTutorial(Tutorial tutorial);
 }
