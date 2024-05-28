e2c686f9475549830de2e9c36282384856f6bf3a
eundeang <111877048+eundeang@users.noreply.github.com>
2024-05-28 20:20:54 +0900

    [FEAT] Quiz todto 구현

--- codekids_BE/src/main/java/oop/codekids/entity/Quiz.java
+++ codekids_BE/src/main/java/oop/codekids/entity/Quiz.java
@@ -1,37 +1,51 @@
 package oop.codekids.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import oop.codekids.Answer;
 import oop.codekids.QuizType;
+import oop.codekids.dto.QuizDto;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Entity
 @Getter
+@Builder
+@AllArgsConstructor
 @NoArgsConstructor
 @Table(name = "quiz")
 public class Quiz {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "quiz_id")
     private int id;
     @Column(name = "quiz_type")
     @Enumerated(EnumType.STRING)
     private QuizType quizType;
     private String title;
     private String description;
-    private int answer;
-    @Builder
-    public Quiz(QuizType quizType, String title, Optional<List<String>> multiChoose, String description, int answer){
-        this.quizType = quizType;
-        this.title = title;
-        this.description = description;
-        this.answer = answer;
-    }
-    @OneToMany(mappedBy = "quiz")
+    @Enumerated(EnumType.STRING)
+    private Answer answer;
+
+    @OneToMany(mappedBy = "quiz", fetch =  FetchType.LAZY)
     private List<MultiChoose> multiChooseList = new ArrayList<>();
 
+    @ManyToOne
+    @JoinColumn(name = "tutorial")
+    private Tutorial tutorial;
+
+    public QuizDto toDto(){
+        QuizDto quizDto = QuizDto.builder()
+                .id(this.id)
+                .title(this.title)
+                .quizType(this.quizType)
+                .answer(this.answer.getAnswer(this.quizType))
+                .description(this.description)
+                .build();
+        return quizDto;
+    }
 }
