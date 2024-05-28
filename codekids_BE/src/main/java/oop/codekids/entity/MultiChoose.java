48b18e6d1f9c8434d3a7bd55b1560229f99a0340
eundeang <111877048+eundeang@users.noreply.github.com>
2024-05-28 13:03:01 +0900

    [FEAT] MultiChoose에 다중선다의 선택지 저장

--- codekids_BE/src/main/java/oop/codekids/entity/MultiChoose.java
+++ codekids_BE/src/main/java/oop/codekids/entity/MultiChoose.java
@@ -1,4 +1,17 @@
 package oop.codekids.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Table(name = "multi_choose")
 public class MultiChoose {
+    @Id
+    private Double id;
+    private int Choice;
 }
