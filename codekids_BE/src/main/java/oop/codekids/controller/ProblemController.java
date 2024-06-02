83376165b46a3e67ce4cc2c709b172c242bab089
eundeang <111877048+eundeang@users.noreply.github.com>
2024-06-02 23:56:36 +0900

    [FEAT] Problem Cont 구성

--- codekids_BE/src/main/java/oop/codekids/controller/ProblemController.java
+++ codekids_BE/src/main/java/oop/codekids/controller/ProblemController.java
@@ -3,6 +3,7 @@ package oop.codekids.controller;
 import lombok.RequiredArgsConstructor;
 import oop.codekids.dto.ProblemDto;
 import oop.codekids.dto.ProblemsDto;
+import oop.codekids.dto.ResponseDto;
 import oop.codekids.service.ProblemService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -28,5 +29,9 @@ public class ProblemController {
     @GetMapping("/getFilteredProblems")
     public ResponseEntity<ProblemsDto> getFilteredProblems(@RequestParam("concept") String concept_s){
         return ResponseEntity.ok(problemService.getFilteredProblemId(concept_s));
+    }
+    @GetMapping("/getOneProblem")
+    public ResponseEntity<ResponseDto> getOneProblem(@RequestParam("id") Long id, @RequestParam("level") int level){
+        return ResponseEntity.ok(problemService.getOneProblem(id,level));
     }
 }
