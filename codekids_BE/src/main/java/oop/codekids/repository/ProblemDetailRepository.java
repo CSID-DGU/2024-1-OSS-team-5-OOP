b93203be884bafba4d7f64686cbe07549bd07378
eundeang <111877048+eundeang@users.noreply.github.com>
2024-06-02 23:55:26 +0900

    [FEAT] 다지선다용 Repo 생성

---
+++ codekids_BE/src/main/java/oop/codekids/repository/ProblemDetailRepository.java
@@ -0,0 +1,14 @@
+package oop.codekids.repository;
+
+import oop.codekids.entity.ProblemDetail;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface ProblemDetailRepository extends JpaRepository<ProblemDetail, Long> {
+    Optional<ProblemDetail> findByProblemIdAndLevel(Long problemId, int level);
+
+//    List<ProblemDetail> findByProblemId(Long problemId);
+}
